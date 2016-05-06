5b0f2556cf4fe4665104bdbab050cc45435dc993
Al- Imran Ahmed <al.imran.cse@gmail.com>
2016-05-06 23:01:11 +0600

    File structure changed

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-#javac -cp "~Documents/java/News/jsoup.jar:" *.java
-#java -cp "~Documents/java/News/jsoup.jar:" App Web News View
-
-javac -cp "./jsoup.jar:" *.java
-java -cp "./jsoup.jar:" App Web News View
+#comblie the java files froom ./src/java
+javac -cp "./lib/jsoup.jar:" ./src/java/*
+#move all clsss files form ./src/java to ./src/class
+mv ./src/java/*.class ./src/class/
+#Run all class files
+cd src/class
+java -cp "../../lib/jsoup.jar:" App News View Web
