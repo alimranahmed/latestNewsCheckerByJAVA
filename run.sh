b605d416b2825fa421c51777fb473a5474e5b0e9
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-02-02 01:02:11 +0600

    Typo fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#comblie the java files froom ./src/java
+#comblie the java files from ./src/java
 javac -cp "./lib/jsoup.jar:" ./src/java/*
 #move all clsss files form ./src/java to ./src/class
 mv ./src/java/*.class ./src/class/
-#Run all class files
+#Run all class files	
 cd src/class
 java -cp "../../lib/jsoup.jar:" App News View Web
